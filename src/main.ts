238b27984a2391746fac414d41fa43abad839af6
trigun <imlerich222@gmail.con>
2025-07-18 12:59:24 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Plugins
+import { createApp } from 'vue'
+import App from './App.vue'
+import { isPageLoading } from '@/utils/loading.ts'
 import { registerPlugins } from '@/plugins'
 import { registerSW } from 'virtual:pwa-register'
+import router from '@/router'
+
 registerSW({ immediate: true })
-
-// Components
-import App from './App.vue'
-
-// Composables
-import { createApp } from 'vue'
-
-// Styles
-import 'unfonts.css'
 
 const app = createApp(App)
 
-registerPlugins(app)
+router.beforeEach((to, from, next) => {
+  isPageLoading.value = true
+  next()
+})
 
+router.afterEach(() => {
+  setTimeout(() => {
+    isPageLoading.value = false
+  }, 300)
+})
+
+registerPlugins(app)
+app.use(router)
 app.mount('#app')
