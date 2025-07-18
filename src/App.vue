14665f18a9c1a25df0083f2013f2fd3f78254e4a
trigun <imlerich222@gmail.con>
2025-07-18 12:59:24 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <v-app>
+    <v-overlay absolute :value="isPageLoading" z-index="9999">
+      <v-progress-circular indeterminate size="64" width="6" />
+    </v-overlay>
+
     <v-main>
-      <router-view />
+      <router-view v-show="!isPageLoading" />
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts" setup>
-  //
+<script setup lang="ts">
+import { isPageLoading } from '@/utils/loading.ts'
 </script>
